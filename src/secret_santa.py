bf1fa0a2bdc5e2a4a3ea56edc28e3ab424d65bdf
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2025-12-23 15:10:26 -0300

    Otimizando código do secret_santa para ficar mais legível

--- src/secret_santa.py
+++ src/secret_santa.py
@@ -3,47 +3,55 @@ import time
 import random
 from typing import Dict, List
 
+
 class SecretSanta:
     def __init__(
-        self, 
+        self,
         participants: List[str],
-        restrictions: Dict[str, List[str]], 
-        description: str="Amigo Secreto"
+        restrictions: Dict[str, List[str]],
+        description: str = "Amigo Secreto",
     ) -> None:
-        self.__participants = participants
-        self.__restrictions = self.__validate_restrictions(restrictions)
-        self.__description = description
-        self.__results = None
+        self._participants = participants
+        self._restrictions = self._validate_restrictions(restrictions)
+        self._description = description
+        self._results = {}
 
     def __repr__(self):
-        s = f"RESULTADOS PARA {self.__description.upper()}\n\n"
-        for k in sorted(self.__results.keys()):
-            s += f'{k} {(15-len(k))*"-"}> {self.__results[k]}\n'
+        if not self.is_drawn():
+            s = f"SORTEIO AINDA NÃO REALIZADO PARA {self._description.upper()}\n\n"
+            for p in sorted(self._participants):
+                s += f'{p} {(15-len(p))*"-"}> TBD\n'
+
+            return s
+
+        s = f"RESULTADOS PARA {self._description.upper()}\n\n"
+        for k in sorted(self._results.keys()):
+            s += f'{k} {(15-len(k))*"-"}> {self._results[k]}\n'
 
         return s
 
     @property
     def participants(self) -> List[str]:
-        return self.__participants
-    
+        return self._participants
+
     @property
     def restrictions(self) -> Dict[str, List[str]]:
-        return self.__restrictions
+        return self._restrictions
 
     @property
     def description(self) -> str:
-        return self.__description
+        return self._description
 
     @property
     def results(self) -> Dict[str, str]:
-        return self.__results
-    
-    def __validate_restrictions(self, restrictions: Dict[str, List[str]]):
+        return self._results
+
+    def _validate_restrictions(self, restrictions: Dict[str, List[str]]):
         validated_restrictions = {}
 
-        for p in self.__participants:
+        for p in self._participants:
             r_list = restrictions.get(p)
-            
+
             if r_list is None:
                 raise ValueError(
                     "Toda pessoa deve estar na lista de restrições, mesmo que não tenha nenhuma."
@@ -52,20 +60,20 @@ class SecretSanta:
             if not isinstance(r_list, list):
                 raise TypeError(f"As restrições de {p} devem ser uma lista.")
 
-            if not set(r_list).issubset(self.__participants):
+            if not set(r_list).issubset(self._participants):
                 raise ValueError(
                     f"Toda pessoa na lista de restrições de {p} deve ser um participante."
                 )
 
-            validated_restrictions[p] = list(dict.fromkeys(r_list + [p]))  # Por padrão p não pode tirar p
+            validated_restrictions[p] = list(
+                dict.fromkeys(r_list + [p])
+            )  # Por padrão p não pode tirar p
 
         return validated_restrictions
-
 
     def generate_drawing(self, timeout: int = 30) -> Dict[str, str]:
         start_time = time.monotonic()
         results = {}
-
         while True:
             if time.monotonic() - start_time > timeout:
                 raise TimeoutError(
@@ -73,13 +81,15 @@ class SecretSanta:
                     "Isto pode ter acontecido por haver uma restrição impossível."
                 )
 
-            participants_list = self.__participants.copy()
+            participants_list = self._participants.copy()
             available_users = participants_list.copy()
             random.shuffle(participants_list)
 
             results = {}
             for participant in participants_list:
-                possible_users = set(available_users) - set(self.__restrictions[participant])
+                possible_users = set(available_users) - set(
+                    self._restrictions[participant]
+                )
 
                 if not possible_users:  # Não há mais usuários a serem sorteados
                     break
@@ -88,21 +98,21 @@ class SecretSanta:
                 results[participant] = chosen
                 available_users.remove(chosen)
 
-            if len(results) == len(self.__participants):
-                self.__results = results
+            if len(results) == len(self._participants):
+                self._results = results
                 return results
-        
 
-    def export_to_file(self, export_dir: str) -> None:
-        if not os.path.isdir(export_dir):
-            os.makedirs(export_dir)
+    def is_drawn(self) -> bool:
+        return self._results != {}
 
-        for k, v in self.__results.items():
-            with open(
-                f'{export_dir}/{k}-{self.description.replace(" ", "_")}.txt', "w"
-            ) as file:
-                file.write(
-                    f"===============================================================\n"
-                    f"{k}, você tirou o(a) {v} para {self.__description}\n"
-                    f"==============================================================="
-                )
+    def get_result(self, participant: str) -> str:
+        if not self.is_drawn():
+            raise ValueError(
+                "Sorteio ainda não realizado. Execute o método generate_drawing antes de chamar este método."
+            )
+
+        res = self._results.get(participant)
+        if res is None:
+            raise ValueError("Participante inexistente.")
+
+        return res
